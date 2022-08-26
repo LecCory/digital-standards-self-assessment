50d79f0c9c246f28f7e5b96635f3de70087e367f
LecCory <cory.leclair@gmail.com>
2022-08-26 10:53:50 -0400

    more fixes

--- infrastructure/migration/migrate-mongo-config.js
+++ infrastructure/migration/migrate-mongo-config.js
@@ -6,7 +6,7 @@ const config = {
     url: `${process.env.DB_CONNECTION_STRING}`,
 
     // TODO Change this to your database name:
-    databaseName: 'test',
+    databaseName: `${process.env.PROJECT_NAME}-db`,
 
     options: {
       useNewUrlParser: true, // removes a deprecation warning when connecting
@@ -17,20 +17,20 @@ const config = {
   },
 
   // The migrations dir, can be an relative or absolute path. Only edit this when really necessary.
-  migrationsDir: '../../data_migration/migrations',
+  migrationsDir: "../../data_migration/migrations",
 
   // The mongodb collection where the applied changes are stored. Only edit this when really necessary.
-  changelogCollectionName: 'changelog',
+  changelogCollectionName: "changelog",
 
   // The file extension to create migrations and search for in migration dir
-  migrationFileExtension: '.js',
+  migrationFileExtension: ".js",
 
   // Enable the algorithm to create a checksum of the file contents and use that in the comparison to determine
   // if the file should be run.  Requires that scripts are coded to be run multiple times.
   useFileHash: false,
 
   // Don't change this, unless you know what you're doing
-  moduleSystem: 'commonjs',
+  moduleSystem: "commonjs",
 };
 
 module.exports = config;
